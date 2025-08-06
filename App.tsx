8cd25bf671ea0e8d6021c63ec6f0b57aa3c79f2e
Kavivendhan-Shanmugam <kaviprofess999@gmail.com>
2025-08-06 09:09:45 +0530

    Update and rename App.js to App.tsx

--- App.tsx
+++ App.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import "./App.css";
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="hero-section">
-        <h1 className="name">Kavivendhan</h1>
+        <h1 className="name">Kavi Vendhan</h1>
         <p className="tagline">Empowering Ideas | Building the Future</p>
         <button className="cta-button">Explore More</button>
       </header>
     </div>
   );
-}
+};
 
 export default App;
