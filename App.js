f6ff9fc42f50ac188672a3285b4d914561fdd4f9
Kavivendhan-Shanmugam <kaviprofess999@gmail.com>
2025-08-06 09:08:06 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="hero-section">
+        <h1 className="name">Kavivendhan</h1>
+        <p className="tagline">Empowering Ideas | Building the Future</p>
+        <button className="cta-button">Explore More</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
